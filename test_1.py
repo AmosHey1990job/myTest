c24f4396090593ab02f270ec5a324f50da35b636
hey <142470962+AmosHey1990job@users.noreply.github.com>
2023-08-25 14:37:00 +0800

    Create test_1.py

---
+++ test_1.py
@@ -0,0 +1,35 @@
+# Day 1: My Coding Adventure Begins!
+
+# Hey there, fellow coders! 🚀
+# Today marks the beginning of my coding journey in college. I'm stoked!
+
+# First, let's print a welcome message to the console.
+print("Hello, World! 🌍")
+
+# Next, I want to calculate the sum of the first 10 natural numbers.
+total_sum = 0
+for num in range(1, 11):
+    total_sum += num
+
+# Display the result.
+print("The sum of the first 10 natural numbers is:", total_sum)
+
+# Time for a quick break and a snack. Maybe a granola bar? 😄
+
+# Now, let's write a function to check if a number is even or odd.
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+# Test the function with a couple of numbers.
+num1 = 24
+num2 = 17
+print(num1, "is", check_even_odd(num1))
+print(num2, "is", check_even_odd(num2))
+
+# Phew, it's been a productive day of coding and learning. Can't wait for more challenges tomorrow!
+
+# Signing off for now,
+# [Your Name]
