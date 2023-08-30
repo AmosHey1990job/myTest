d72a66be09e28c1c097fe4c9b71a822133377303
hey <142470962+AmosHey1990job@users.noreply.github.com>
2023-08-30 09:12:51 +0800

    Create test_2.py

---
+++ test_2.py
@@ -0,0 +1,32 @@
+# Day 2: Diving Deeper into Coding Waters
+
+# Hey fellow coders! 🖖
+# Day 2 of college coding adventures, and I'm ready to learn more cool stuff.
+
+# Let's start by defining a simple function to calculate the square of a number.
+def calculate_square(number):
+    return number ** 2
+
+# Test the function with a few numbers.
+num1 = 5
+num2 = 8
+print("The square of", num1, "is", calculate_square(num1))
+print("The square of", num2, "is", calculate_square(num2))
+
+# Time for a coffee break! ☕️ Gotta stay caffeinated for coding.
+
+# Now, let's work with lists. I'm creating a list of my favorite fruits.
+favorite_fruits = ["apple", "banana", "orange", "grapes", "strawberry"]
+
+# Print each fruit using a loop.
+print("My favorite fruits:")
+for fruit in favorite_fruits:
+    print("- " + fruit)
+
+# Oh, and I'm learning about version control! I just committed my changes to Git.
+# It feels so pro. 💪
+
+# Wrapping up for now. Looking forward to more coding discoveries tomorrow!
+
+# Happy coding,
+# [Your Name]
