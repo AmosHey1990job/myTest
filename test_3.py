a7735ad06ec5f308f48143e7de3294e3127b2242
hey <142470962+AmosHey1990job@users.noreply.github.com>
2023-08-30 14:55:49 +0800

    Create test_3.py

---
+++ test_3.py
@@ -0,0 +1,39 @@
+# Day 3: Building Mini-Projects and Having Fun
+
+# Hey coding buddies! 🎉
+# Another exciting day of coding exploration. Let's dive in!
+
+# I've started working on a simple calculator app. Let's add two numbers.
+def add_numbers(a, b):
+    return a + b
+
+# Test the calculator function.
+num1 = 10
+num2 = 7
+print("The sum of", num1, "and", num2, "is:", add_numbers(num1, num2))
+
+# It's snack time again. Anyone up for some pretzels? 🥨
+
+# Next, I'm creating a basic text-based game: Rock-Paper-Scissors.
+player_choice = input("Enter your choice (rock/paper/scissors): ")
+
+# The opponent's choice could be generated randomly, but let's keep it simple.
+opponent_choice = "rock"
+
+# Determine the winner.
+if player_choice == opponent_choice:
+    result = "It's a tie!"
+elif player_choice == "rock" and opponent_choice == "scissors":
+    result = "You win!"
+else:
+    result = "You lose!"
+
+# Display the result.
+print(result)
+
+# Learning about functions has been mind-blowing. I'm starting to see how everything fits together.
+
+# Alright, time to relax with some music before I call it a day. 🎶
+
+# Stay curious and keep coding,
+# [Your Name]
